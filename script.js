bd5c4f738a81122dac3299674e373e8c1a33ffa9
Nilou <desnillight@gmail.com>
2024-11-30 21:59:39 +0330

    adding the dice and adding status to js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+//========== status =========
+let newGameBtn = document.querySelector('#new-game');
+let rollDiceBtn = document.querySelector('#roll-dice');
+let holdBtn = document.querySelector('#hold');
+let positionedBtn = document.querySelectorAll('.positioned-btn');
+let dice = document.querySelector('.dice');
+let score = document.querySelector('.score');
+let currentScore = document.querySelector('.current-score');
