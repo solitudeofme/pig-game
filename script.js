b3ad843c364218b2076fb9b88775a014d9754c84
Nilou <desnillight@gmail.com>
2024-11-30 22:44:04 +0330

    rolling the dice event

--- script.js
+++ script.js
@@ -6,5 +6,12 @@ let rollDiceBtn = document.querySelector('#roll-dice');
 let holdBtn = document.querySelector('#hold');
 let positionedBtn = document.querySelectorAll('.positioned-btn');
 let dice = document.querySelector('.dice');
-let score = document.querySelector('.score');
-let currentScore = document.querySelector('.current-score');
+let score = document.querySelector('.score').textContent;
+let currentScore = document.querySelector('.current-score').textContent;
+let diceNum = 0;
+let sum;
+rollDiceBtn.addEventListener('click', () => {
+  diceNum = Math.trunc(Math.random() * 6) + 1;
+  dice.classList.remove('hidden');
+  dice.src = `images/dice-${diceNum}.png`;
+});
