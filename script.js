04198450dada23e827bcd860dcc0f76389485f75
Nilou <desnillight@gmail.com>
2024-12-01 22:07:21 +0330

    adding a function to determine the winner

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 'use strict';
+
+//========== status =========
+
+let newGameBtn = document.querySelector('#new-game');
+let rollDiceBtn = document.querySelector('#roll-dice');
+let holdBtn = document.querySelector('#hold');
+let dice = document.querySelector('.dice');
+let score1 = document.querySelector('#score-1');
+let score2 = document.querySelector('#score-2');
+let currentScore1 = document.querySelector('#current-score-1');
+let currentScore2 = document.querySelector('#current-score-2');
+let player1 = document.querySelector('.player-1');
+let player2 = document.querySelector('.player-2');
+
 //========== functions =======
 let c = 1;
+
 const switchPlayer = () => {
   if (c % 2 === 1) {
     player1.classList.add('player-disable');
@@ -10,30 +25,30 @@ const switchPlayer = () => {
     player1.classList.remove('player-disable');
   }
   c++;
-
-  // if (player1.classList.contains('player-disable')) {
-  //   player1.classList.remove('player-disable');
-  //   player2.classList.add('player-disable');
-  // } else if (player2.classList.contains('player-disable')) {
-  //   player2.classList.remove('player-disable');
-  //   player1.classList.add('player-disable');
-  // }
 };
-//========== status =========
-let newGameBtn = document.querySelector('#new-game');
-let rollDiceBtn = document.querySelector('#roll-dice');
-let holdBtn = document.querySelector('#hold');
-let positionedBtn = document.querySelectorAll('.positioned-btn');
-let dice = document.querySelector('.dice');
-let score1 = document.querySelector('#score-1');
-let score2 = document.querySelector('#score-2');
-let currentScore1 = document.querySelector('#current-score-1');
-let currentScore2 = document.querySelector('#current-score-2');
-let player1 = document.querySelector('.player-1');
-let player2 = document.querySelector('.player-2');
+
+let disablingBtns = () => {
+  rollDiceBtn.disabled = true;
+  holdBtn.disabled = true;
+  dice.classList.add('hidden');
+};
+
+let whoIsWinner = () => {
+  console.log(score1.textContent);
+  if (Number(score1.textContent) >= 10) {
+    player1.classList.add('player--winner');
+    player2.classList.add('player--loser');
+    disablingBtns();
+  } else if (Number(score2.textContent) >= 10) {
+    player2.classList.add('player--winner');
+    player1.classList.add('player--loser');
+    disablingBtns();
+  }
+};
 
 //========== Events =========
 let diceNum = 0;
+
 rollDiceBtn.addEventListener('click', () => {
   diceNum = Math.trunc(Math.random() * 6) + 1;
   dice.classList.remove('hidden');
@@ -56,14 +71,18 @@ rollDiceBtn.addEventListener('click', () => {
 
 holdBtn.addEventListener('click', () => {
   if (c % 2 === 1) {
-    score1.textContent = currentScore1.textContent;
+    score1.textContent =
+      Number(score1.textContent) + Number(currentScore1.textContent);
     currentScore1.textContent = '0';
   } else {
-    score2.textContent = currentScore2.textContent;
+    score2.textContent =
+      Number(score2.textContent) + Number(currentScore2.textContent);
     currentScore2.textContent = '0';
   }
+  whoIsWinner();
   switchPlayer();
 });
+
 newGameBtn.addEventListener('click', () => {
   currentScore1.textContent = '0';
   currentScore2.textContent = '0';
