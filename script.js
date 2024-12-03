5108e83d6832368d4cfbc9891f052e3f91ad9e16
Nilou <desnillight@gmail.com>
2024-12-03 20:07:40 +0330

    improving the performance

--- script.js
+++ script.js
@@ -6,25 +6,21 @@ let newGameBtn = document.querySelector('#new-game');
 let rollDiceBtn = document.querySelector('#roll-dice');
 let holdBtn = document.querySelector('#hold');
 let dice = document.querySelector('.dice');
+let score0 = document.querySelector('#score-0');
 let score1 = document.querySelector('#score-1');
-let score2 = document.querySelector('#score-2');
+let currentScore0 = document.querySelector('#current-score-0');
 let currentScore1 = document.querySelector('#current-score-1');
-let currentScore2 = document.querySelector('#current-score-2');
+let player0 = document.querySelector('.player-0');
 let player1 = document.querySelector('.player-1');
-let player2 = document.querySelector('.player-2');
 
 //========== functions =======
-let c = 1;
-
+let activePlayer = 0;
 const switchPlayer = () => {
-  if (c % 2 === 1) {
-    player1.classList.add('player-disable');
-    player2.classList.remove('player-disable');
-  } else {
-    player2.classList.add('player-disable');
-    player1.classList.remove('player-disable');
-  }
-  c++;
+  currentScore = 0;
+  document.getElementById(`current-score-${activePlayer}`).textContent = '0';
+  activePlayer = !activePlayer ? 1 : 0;
+  player0.classList.toggle('player-disable');
+  player1.classList.toggle('player-disable');
 };
 
 let disablingBtns = () => {
@@ -34,67 +30,54 @@ let disablingBtns = () => {
 };
 
 let whoIsWinner = () => {
-  console.log(score1.textContent);
-  if (Number(score1.textContent) >= 100) {
-    player1.classList.add('player--winner');
-    player2.classList.add('player--loser');
-    disablingBtns();
-  } else if (Number(score2.textContent) >= 100) {
-    player2.classList.add('player--winner');
-    player1.classList.add('player--loser');
+  if (document.getElementById(`score-${activePlayer}`).textContent >= 100) {
+    document
+      .querySelector(`.player-${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player-${Number(!activePlayer)}`)
+      .classList.add('player--loser');
     disablingBtns();
   }
 };
 
 //========== Events =========
-let diceNum = 0;
+let currentScore = 0;
 
 rollDiceBtn.addEventListener('click', () => {
-  diceNum = Math.trunc(Math.random() * 6) + 1;
+  let diceNum = Math.trunc(Math.random() * 6) + 1;
   dice.classList.remove('hidden');
   dice.src = `images/dice-${diceNum}.png`;
   if (diceNum === 1) {
-    if (c % 2 === 1) {
-      currentScore1.textContent = '0';
-    } else {
-      currentScore2.textContent = '0';
-    }
     switchPlayer();
   } else {
-    if (c % 2 === 1) {
-      currentScore1.textContent = Number(currentScore1.textContent) + diceNum;
-    } else {
-      currentScore2.textContent = Number(currentScore2.textContent) + diceNum;
-    }
+    currentScore += diceNum;
+    document.getElementById(`current-score-${activePlayer}`).textContent =
+      currentScore;
   }
 });
 
 holdBtn.addEventListener('click', () => {
-  if (c % 2 === 1) {
-    score1.textContent =
-      Number(score1.textContent) + Number(currentScore1.textContent);
-    currentScore1.textContent = '0';
-  } else {
-    score2.textContent =
-      Number(score2.textContent) + Number(currentScore2.textContent);
-    currentScore2.textContent = '0';
-  }
+  document.getElementById(`score-${activePlayer}`).textContent =
+    Number(document.getElementById(`score-${activePlayer}`).textContent) +
+    currentScore;
   whoIsWinner();
   switchPlayer();
 });
 
 newGameBtn.addEventListener('click', () => {
-  c = 0;
-  switchPlayer();
+  activePlayer = 0;
+  player0.classList.remove('player-disable');
+  player1.classList.add('player-disable');
+  currentScore0.textContent = '0';
   currentScore1.textContent = '0';
-  currentScore2.textContent = '0';
+  score0.textContent = '0';
   score1.textContent = '0';
-  score2.textContent = '0';
   rollDiceBtn.disabled = false;
   holdBtn.disabled = false;
   dice.classList.add('hidden');
+  player0.classList.remove('player--winner');
+  player0.classList.remove('player--loser');
   player1.classList.remove('player--winner');
   player1.classList.remove('player--loser');
-  player2.classList.remove('player--winner');
-  player2.classList.remove('player--loser');
 });
